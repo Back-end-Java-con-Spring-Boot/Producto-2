75b3454cad69b44f7e01040b1aa010d1d6c6507a
Mar <marbou96@hotmail.com>
2026-03-11 15:49:23 +0100

    Vistas de vehículos y Reserva Ahora, en vehículos se crean las paginas admin y formulario para editar y crear. Se añaden los campos imagen y nombre al modelo vehículo

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,15 +7,15 @@ VALUES (1, 'A todo gas', 'Calle Gran Vía 12', '910000001', 'Madrid'),
        (5, 'Heliopuerto Esperanza', 'Zona Fórum s/n', '930000005', 'Barcelona');
 
 -- 2. VEHÍCULOS (ACTIVO: Tienes la entidad Vehiculo)
-INSERT IGNORE INTO vehiculos (id, matricula, plazas, precio_hora, tipo_vehiculo, sucursal_id)
-VALUES (1, '1234ABC', 5, 45.50, 'TERRESTRE', 1),
-       (2, '5678DEF', 9, 80.00, 'TERRESTRE', 1),
-       (3, '9012GHI', 5, 55.00, 'TERRESTRE', 2),
-       (4, '3456JKL', 2, 35.00, 'TERRESTRE', 2),
-       (5, '7ª-VA-1-23', 2, 150.00, 'MARITIMO', 4),
-       (6, '6ª-VA-2-23', 8, 950.00, 'MARITIMO', 4),
-       (7, 'EC-ABC', 4, 1200.00, 'AEREO', 3),
-       (8, 'EC-XYZ', 6, 2500.00, 'AEREO', 5);
+INSERT IGNORE INTO vehiculos (id, nombre, matricula, plazas, precio_hora, tipo_vehiculo, sucursal_id, imagen_url)
+VALUES (1, 'coche1', '1234ABC',  5, 45.50, 'TERRESTRE', 1, 'https://images.pexels.com/photos/1592384/pexels-photo-1592384.jpeg?_gl=1*1f9hg0w*_ga*MTQwOTk3MzY3OS4xNzczMjM5NDA0*_ga_8JE65Q40S6*czE3NzMyMzk0MDQkbzEkZzEkdDE3NzMyMzk1ODYkajQzJGwwJGgw'),
+       (2, 'coche2', '5678DEF', 9, 80.00, 'TERRESTRE', 1, 'https://images.pexels.com/photos/12008571/pexels-photo-12008571.jpeg?_gl=1*uoz85t*_ga*MTQwOTk3MzY3OS4xNzczMjM5NDA0*_ga_8JE65Q40S6*czE3NzMyMzk0MDQkbzEkZzEkdDE3NzMyMzk2OTMkajU5JGwwJGgw'),
+       (3, 'coche3', '9012GHI', 5, 55.00, 'TERRESTRE', 2, 'https://images.pexels.com/photos/2071/broken-car-vehicle-vintage.jpg?_gl=1*1usk8au*_ga*MTQwOTk3MzY3OS4xNzczMjM5NDA0*_ga_8JE65Q40S6*czE3NzMyMzk0MDQkbzEkZzEkdDE3NzMyMzk3MjkkajIzJGwwJGgw'),
+       (4, 'moto1', '3456JKL', 2, 35.00, 'TERRESTRE', 2, 'https://images.pexels.com/photos/2611691/pexels-photo-2611691.jpeg?_gl=1*ytdvxw*_ga*MTQwOTk3MzY3OS4xNzczMjM5NDA0*_ga_8JE65Q40S6*czE3NzMyMzk0MDQkbzEkZzEkdDE3NzMyMzk3ODckajI1JGwwJGgw'),
+       (5, 'barco1', '7ª-VA-1-23',  2, 150.00, 'MARITIMO', 4, 'https://images.pexels.com/photos/86699/fishing-boat-denmark-beach-sea-86699.jpeg?_gl=1*mq3f4t*_ga*MTQwOTk3MzY3OS4xNzczMjM5NDA0*_ga_8JE65Q40S6*czE3NzMyMzk0MDQkbzEkZzEkdDE3NzMyMzk4NTEkajIyJGwwJGgw'),
+       (6, 'barco2', '6ª-VA-2-23', 8, 950.00, 'MARITIMO', 4, 'https://images.pexels.com/photos/163236/luxury-yacht-boat-speed-water-163236.jpeg?_gl=1*3lpbxf*_ga*MTQwOTk3MzY3OS4xNzczMjM5NDA0*_ga_8JE65Q40S6*czE3NzMyMzk0MDQkbzEkZzEkdDE3NzMyMzk4MTgkajU1JGwwJGgw'),
+       (7, 'avion1', 'EC-ABC',4, 1200.00, 'AEREO', 3, 'https://images.pexels.com/photos/78786/fighter-jet-jet-lockheed-martin-f-35-lightning-ii-2011-78786.jpeg?_gl=1*1nwtocc*_ga*MTQwOTk3MzY3OS4xNzczMjM5NDA0*_ga_8JE65Q40S6*czE3NzMyMzk0MDQkbzEkZzEkdDE3NzMyMzk5MTYkajI1JGwwJGgw'),
+       (8, 'avion2', 'EC-XYZ', 6, 2500.00, 'AEREO', 5, 'https://images.pexels.com/photos/1486842/pexels-photo-1486842.jpeg?_gl=1*1cb3fjp*_ga*MTQwOTk3MzY3OS4xNzczMjM5NDA0*_ga_8JE65Q40S6*czE3NzMyMzk0MDQkbzEkZzEkdDE3NzMyMzk4ODYkajU1JGwwJGgw');
 
 /* BLOQUE COMENTADO:
    Las siguientes tablas darían error porque aún no tienes las clases Java (Entities)
